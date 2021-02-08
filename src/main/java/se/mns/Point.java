e66068c7aea73655506023887bddbdf0b439db6c
Naoya Irikura <naoiri2001@skola.goteborg.se>
2021-02-08 14:35:24 +0100

    Created Point class which calculates even highest point

---
+++ src/main/java/se/mns/Point.java
@@ -0,0 +1,26 @@
+package se.mns;
+
+public class Point {
+	
+	private int latestPoint;
+	private int highestPoint;
+	
+	public Point() {
+		this.latestPoint = 0;
+		this.highestPoint = 0;
+	}
+	
+	public int getLatestPoints() {
+		return this.latestPoint;
+	}
+	
+	public void pointIncrease() {
+		this.latestPoint++;
+	}
+	
+	public void setHighestPoint() {
+		if(this.latestPoint > this.highestPoint) {
+			this.highestPoint = this.latestPoint;
+		}
+	}
+}
