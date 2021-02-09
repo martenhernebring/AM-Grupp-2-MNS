0f4298b6f15d67dfdf7190e656d51a3a3ec3ff3c
suzyrgo <suztra0803@skola.goteborg.se>
2021-02-09 21:47:28 +0100

    Created a new class Grafic.java

---
+++ src/main/java/se/mns/Grafic.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+
+import javax.swing.*;
+public class Grafic extends Bird{
+
+    public void paintShape(Grafics g) {
+        super.paintShape(g);
+        this.setBackgrond(Color.DARK_GRAY);
+
+        Grafics2D g2D = (Grafics2D) g;
+        int[]xPoints = {50,100,150,200,250,300,350};
+        int[]yPoints = {350,250,250,200,150,250,350};
+        int nPoints = xPoints.length;
+        int[] x = {100,200,300};
+        int [] y = {100, 200, 300};
+
+//* Drawing a Polygram*/
+        g2D.setColor(Color.ORANGE);
+        g2D.drawPolyline(xPoints, yPoints, nPoints);
+        g2D.set.Stroke(new BasicStoke(20));
+        g2D.drawLine(200, 200, 400, 400);
+
+
+        g2D.setColor(Color.GREEN);
+        g2D.drawPolygon(x, y,3);
+        g2D.fillPolygon(x,y,3);
+    }
+}
