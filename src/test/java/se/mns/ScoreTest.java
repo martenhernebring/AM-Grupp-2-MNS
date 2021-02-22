96c5d25b23c641952c751da1c3daf55276c824fc
Marten Hernebring <marten.hernebring@gmail.com>
2021-02-22 13:45:16 +0100

    Fixed conflict in test score

--- src/test/java/se/mns/ScoreTest.java
+++ src/test/java/se/mns/ScoreTest.java
@@ -9,62 +9,28 @@ class scoreTest {
 	Score score = new Score();
 	
 	@Test
-	public void testConstructor() {
-<<<<<<< HEAD
-		//assertEquals(0, score.getLatest());
-	}
-	
-	@Test
 	public void testGetLatestscores() {
-		//assertEquals(0, score.getLatest());
+		assertEquals(0, score.getLatest());
 	}
 	
 	@Test
 	public void testscoreIncrease() {
 		score.increase();
-		//assertEquals(1, score.getLatest());
+		assertEquals(1, score.getLatest());
 	}
 	
 	@Test
 	public void testResetscores() {
 		score.increase();
 		score.reset();
-		//assertEquals(0, score.getLatest());
+		assertEquals(0, score.getLatest());
 	}
 
 	@Test
 	public void testSetHigestscore() {
 		score.increase();
-		//score.setHighest();
-		//assertEquals(1, score.getHighest());
-=======
-		assertEquals(0, point.getLatest());
-	}
-	
-	@Test
-	public void testGetLatestPoints() {
-		assertEquals(0, point.getLatest());
-	}
-	
-	@Test
-	public void testPointIncrease() {
-		point.increase();
-		assertEquals(1, point.getLatest());
-	}
-	
-	@Test
-	public void testResetPoints() {
-		point.increase();
-		point.reset();
-		assertEquals(0, point.getLatest());
-	}
-
-	@Test
-	public void testSetHigestPoint() {
-		point.increase();
-		point.update();
-		assertEquals(1, point.getHighest());
->>>>>>> save
+		score.update();
+		assertEquals(1, score.getHighest());
 	}
 
 	
