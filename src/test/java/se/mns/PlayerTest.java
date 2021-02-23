8479de36a46d689eb3f48e907491ca7110beb0c6
Naoya Irikura <naoiri2001@skola.goteborg.se>
2021-02-23 15:34:18 +0100

    Added noSpaceAllowed and noNumbersAllowed in PlayerTest

--- src/test/java/se/mns/PlayerTest.java
+++ src/test/java/se/mns/PlayerTest.java
@@ -5,13 +5,30 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
-
-	Player player = new Player("Naoya", 10);
+	
+	Player player;
+	
+	@Test
+	public void noSpaceAllowedInName() {
+		 
+		assertThrows(IllegalArgumentException.class, ()->{
+			player = new Player(" N a o y a ", 10);
+		});
+	}
+	
+	@Test
+	public void noNumberAllowedInName() {
+		 
+		assertThrows(IllegalArgumentException.class, ()->{
+			player = new Player("Naoya1", 10);
+		});
+	}
 	
 	@Test
 	public void compareToTest() {
-		Player player2 = new Player("Naoya2", 20);
-		assertTrue(player.compareTo(player2)<0);
+		Player player1 = new Player("Naoya", 10);
+		Player player2 = new Player("Naoya", 20);
+		assertTrue(player1.compareTo(player2)<0);
 	}
-
+	
 }
