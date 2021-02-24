151fa6193650a1baed940d94aa2625dab59a6170
Marten Hernebring <marten.hernebring@gmail.com>
2021-02-24 13:37:55 +0100

    Put save and score together and removed time class

--- src/main/java/se/mns/Score.java
+++ src/main/java/se/mns/Score.java
@@ -1,5 +1,12 @@
 package se.mns;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 public class Score {
 	
 	private int latest;
@@ -31,4 +38,38 @@ public class Score {
 	public void reset() {
 		latest = 0;
 	}
+	
+	public String latest() {
+        return "Latest Score: " + Integer.toString(getLatest());
+    }
+    
+    public String highest() {
+        return "Highest Score: " + Integer.toString(getHighest());
+    }
+    
+    public void write(String fileName) throws IOException {
+        Path target = Path.of(fileName).toAbsolutePath().normalize();
+        try (BufferedWriter writer = Files.newBufferedWriter(target, StandardOpenOption.CREATE)) {
+            writer.write(latest());
+            writer.write("\n");
+            writer.write(highest());
+        }
+    }
+    
+    public String[] read(String fileName) throws IOException {
+        Path source = Path.of(fileName);
+        if (!Files.isReadable(source)) {
+            throw new IllegalArgumentException("The file %s is not readable.%n" + fileName);
+        }
+        String[] outputLines = new String[2];
+        try (BufferedReader reader = Files.newBufferedReader(source)) {
+            String line;
+            int index = 0;
+            while ((line = reader.readLine()) != null) {
+                outputLines[index] = line;
+                index++;
+            }
+        }
+        return outputLines;
+    }
 }
--- src/test/java/se/mns/ScoreTest.java
+++ src/test/java/se/mns/ScoreTest.java
@@ -2,6 +2,10 @@ package se.mns;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.Test;
 
 class scoreTest {
@@ -33,6 +37,57 @@ class scoreTest {
 		assertEquals(1, score.getHighest());
 	}
 
-	
+	@Test
+    public void latestTest() {
+        assertEquals("Latest Score: 0", score.latest());
+    }
+    
+    @Test
+    public void highestTest() {
+        assertEquals("Highest Score: 0", score.highest());
+    }
+    
+    @Test
+    public void increaseTest() {
+        score.increase();
+        assertEquals("Latest Score: 1", score.latest());
+    }
+    
+    @Test
+    public void updateTest() {
+        score.increase();
+        score.update();
+        assertEquals("Highest Score: 1", score.highest());
+    }
+    
+    @Test
+    public void resetTest() {
+        score.increase();
+        score.reset();
+        assertEquals("Latest Score: 0", score.latest());
+    }
+    
+    @Test
+    public void writeTest() {
+        try {
+            Path outFile =  Path.of("test.txt").toAbsolutePath().normalize(); 
+            if (Files.exists(outFile)) {
+                System.err.printf("The file %s already exists.%n", outFile);
+                fail();
+            } else {
+                score.write("test.txt");
+                if (!Files.exists(outFile)) {
+                    System.err.printf("The file %s does not exist.%n", outFile);
+                    fail();
+                } else {
+                    Files.delete(outFile);
+                }     
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            fail();
+        }
+    }
 
 }
