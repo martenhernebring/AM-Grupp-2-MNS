a435507804068c5286b95d0d8f31a38df0bec87a
Marten Hernebring <marten.hernebring@gmail.com>
2021-02-24 16:54:53 +0100

    Added comment jtextfield

--- src/main/java/se/mns/SimplePane.java
+++ src/main/java/se/mns/SimplePane.java
@@ -1,6 +1,7 @@
 package se.mns;
 
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 
 public class SimplePane {
 
@@ -8,6 +9,10 @@ public class SimplePane {
         String name = JOptionPane.showInputDialog("What is your name?");
         String greeting = "Welcome " + name;
         System.out.println(greeting);
+        
+        /*final JTextField pf = new JTextField();
+        password = JOptionPane.showConfirmDialog(null, pf, "Ange lösenord", JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION ? new String(pf.getPassword()): "";*/
     }
 
 }
